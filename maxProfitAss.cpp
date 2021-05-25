b353ed36ead827a36d50c3bf22fd40de15c21955
hitenSharm <hiten.sharm1708@gmail.com>
2021-05-25 11:27:04 +0530

    some questions

---
+++ maxProfitAss.cpp
@@ -0,0 +1,31 @@
+#pragma GCC optimize("Ofast")  
+#pragma GCC target("avx,avx2,fma") 
+static auto _ = [] ()
+{ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);return 0;}();
+
+class Solution {
+public:
+    int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit, vector<int>& worker) {
+        sort(worker.begin(),worker.end());
+        int i=0;int ans=0;int j=0;      
+        vector<pair<int,int>>vec;
+        for(int i=0;i<difficulty.size();i++)
+        {
+        	vec.push_back(make_pair(difficulty[i],profit[i]));
+        }
+        sort(vec.begin(),vec.end());
+        int mxProfit=0;
+        while(i<worker.size())
+        {            
+        	while(j<vec.size() && vec[j].first<=worker[i])
+        	{                
+        		mxProfit=max(mxProfit,vec[j].second); 
+                j++;
+        	}                        
+        	ans+=mxProfit;
+        	i++;
+        }
+        return ans;
+
+    }
+};
---
+++ subaaryProdLessThanK.cpp
@@ -0,0 +1,48 @@
+#pragma GCC optimize("Ofast")  
+#pragma GCC target("avx,avx2,fma") 
+static auto _ = [] ()
+{ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);return 0;}();
+
+class Solution {
+public:
+    int numSubarrayProductLessThanK(vector<int>& nums, int k) {
+        long i=0;long j=0;
+        long n=nums.size();
+        long prod=1;
+        long ans=0;
+        long pj=0;
+        long fl=0;        
+        while(i<n && j<n)
+        {        	
+        	while(j<n && (prod*nums[j])<k)
+        	{
+        		prod=prod*nums[j];                
+        		j++;         		
+        	}
+        	long len=j-i;            
+        	long temp1=len*(len+1)/2;
+        	if(fl==0)
+        	{                                
+        		fl=1;
+        		ans+=temp1;
+        	}
+        	else
+        	{
+        		long len2=pj-i+1;
+        		long temp2=len2*(len2+1)/2;
+        		ans=ans+(temp1-temp2);
+        	}
+        	prod=prod/nums[i];
+        	i++;
+        	if(i>j)
+            {
+                fl=0;
+                j=i;
+                prod=1;
+            }            
+            pj=j-1;    
+            
+        }
+        return ans;
+    }
+};
