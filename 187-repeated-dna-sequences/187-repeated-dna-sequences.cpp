0615849f30310193a9ad03e0ce79571f61e9fee7
Hiten Sharma <56029311+hitenSharm@users.noreply.github.com>
2022-01-04 11:01:41 +0530

    Time: 151 ms (8.66%), Space: 23.4 MB (32.73%) - LeetHub

--- 187-repeated-dna-sequences/187-repeated-dna-sequences.cpp
+++ 187-repeated-dna-sequences/187-repeated-dna-sequences.cpp
@@ -1,3 +1,12 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+static auto _ = []() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+  return 0;
+}();
+
 class Solution {
 public:
     vector<string> findRepeatedDnaSequences(string s) {
