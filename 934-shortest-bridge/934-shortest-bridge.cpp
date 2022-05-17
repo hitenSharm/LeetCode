5d62b0f0ae815ad55bcbc5ff7fc093842964039a
Hiten Sharma <56029311+hitenSharm@users.noreply.github.com>
2022-05-17 15:01:22 +0530

    Time: 41 ms (94.86%), Space: 19.1 MB (80.12%) - LeetHub

--- 934-shortest-bridge/934-shortest-bridge.cpp
+++ 934-shortest-bridge/934-shortest-bridge.cpp
@@ -1,3 +1,8 @@
+#pragma GCC optimize("Ofast")  
+#pragma GCC target("avx,avx2,fma") 
+static auto _ = [] ()
+{ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);return 0;}();
+
 class Solution {
 public:
 	int dirs[5]={-1,0,1,0,-1};
