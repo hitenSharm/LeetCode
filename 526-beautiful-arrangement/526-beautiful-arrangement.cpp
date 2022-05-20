937eba173883b5041239a882816b72bd4e1724b2
Hiten Sharma <56029311+hitenSharm@users.noreply.github.com>
2022-05-20 14:24:35 +0530

    Time: 621 ms (12.12%), Space: 137.2 MB (5.17%) - LeetHub

---
+++ 526-beautiful-arrangement/526-beautiful-arrangement.cpp
@@ -0,0 +1,38 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+static auto _ = []() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+  return 0;
+}();
+
+class Solution {
+public:
+
+	void recu(int& ans,vector<int>vis,int& n,int curr)
+	{
+		if(curr>n)
+		{
+			ans++;
+			return ;
+		}		
+		for(int i=1;i<=n;i++)
+		{
+			if(vis[i]==0 and (i%curr==0 || curr%i==0))
+			{
+				vis[i]=1;
+				recu(ans,vis,n,curr+1);
+				vis[i]=0;
+			}
+		}
+		
+	}
+
+    int countArrangement(int n) {
+        vector<int>vis(n+1,0);
+		int ans=0;		
+		recu(ans,vis,n,1);
+		return ans;
+    }
+};
