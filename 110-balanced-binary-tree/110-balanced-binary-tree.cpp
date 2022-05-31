0b4dd2e7bba931e465dba7ac423209a9294e6680
Hiten Sharma <56029311+hitenSharm@users.noreply.github.com>
2022-05-31 11:39:43 +0530

    Time: 24 ms (26.73%), Space: 21 MB (26.29%) - LeetHub

---
+++ 110-balanced-binary-tree/110-balanced-binary-tree.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int fl=0;
+	int recu(TreeNode* root)
+	{
+		if(!root)return 0;
+		int l=1+recu(root->left);
+		int r=1+recu(root->right);
+		if(abs(l-r)>1)fl=1;
+		return max(l,r);
+	}
+
+    bool isBalanced(TreeNode* root) {
+        recu(root);
+		if(fl==1)return false;
+		return true;
+    }
+};
