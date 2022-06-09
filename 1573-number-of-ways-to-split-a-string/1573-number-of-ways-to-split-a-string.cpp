f452b5ca47da6d5b818cdeadf6dfbbbf751f39b7
Hiten Sharma <56029311+hitenSharm@users.noreply.github.com>
2022-06-09 19:10:34 +0530

    Time: 34 ms (83.33%), Space: 14.1 MB (27.90%) - LeetHub

--- 1573-number-of-ways-to-split-a-string/1573-number-of-ways-to-split-a-string.cpp
+++ 1573-number-of-ways-to-split-a-string/1573-number-of-ways-to-split-a-string.cpp
@@ -14,16 +14,18 @@ public:
 		for(int i=0;i<s.size();i++)
 			if(s[i]=='1')oc++;
 		if(oc%3!=0)return 0;
-		if(oc==0)return (((s.size()-1)*(s.size()-2)/2)%mod);
-		vector<int>freq(s.size());
+		if(oc==0)return (((s.size()-1)*(s.size()-2)/2)%mod);		
 		int cnt=0;
+        long long int t1=0;
+		long long int t2=0;
 		for(int i=0;i<s.size();i++)
 		{
-			if(s[i]=='1')cnt++;
-			freq[i]=cnt;
+			if(s[i]=='1')cnt++;			
+            if(cnt==(1*(oc/3)))t1++;
+            if(cnt==(2*(oc/3)))t2++;
+            if(cnt>(2*(oc/3)))break;
 		}
-		long long int t1=count(freq.begin(),freq.end(),1*(oc/3));
-		long long int t2=count(freq.begin(),freq.end(),2*(oc/3));
+		
 		return ((t1*t2)%mod);
     }
 };
