5b709a335797ef1d8006c26c12fbc43b0d03bd0b
Hiten Sharma <56029311+hitenSharm@users.noreply.github.com>
2022-07-16 18:35:39 +0530

    Time: 27 ms (6.81%), Space: 14.2 MB (36.13%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -1,33 +1,27 @@
-#pragma GCC optimize("Ofast")
-#pragma GCC target("avx,avx2,fma")
-static auto _ = []() {
-  ios_base::sync_with_stdio(false);
-  cin.tie(nullptr);
-  cout.tie(nullptr);
-  return 0;
-}();
 class Solution {
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        if(image[sr][sc]==color)return image;
+        int toChange=image[sr][sc];
         queue<pair<int,int>>q;
         q.push({sr,sc});
-        int start=image[sr][sc];
         image[sr][sc]=color;
         int dirs[5]={-1,0,1,0,-1};
-        while(!q.empty() and start!=color)
+        while(!q.empty())
         {
-            pair<int,int>& tmp=q.front();
-            for(int k=0;k<4;++k)
+            pair<int,int>p = q.front();
+            q.pop();
+            int i=p.first,j=p.second;
+            for(int k=0;k<4;k++)
             {
-                int nr=tmp.first+dirs[k],nc=tmp.second+dirs[k+1];
-                if(nr>=0 and nc>=0 and nr<image.size() and nc<image[0].size() and image[nr][nc]==start)
+                int ni=i+dirs[k],nj=j+dirs[k+1];
+                if(ni>=0 and nj>=0 and ni<image.size() and nj<image[0].size() and image[ni][nj]==toChange)
                 {
-                    image[nr][nc]=color;
-                    q.push({nr,nc});
+                    image[ni][nj]=color;
+                    q.push({ni,nj});
                 }
             }
-            q.pop();
         }
         return image;
-    }    
+    }
 };
