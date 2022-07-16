1bd6329dbe9183fd260cd09e61b8d05d524f1d3e
Hiten Sharma <56029311+hitenSharm@users.noreply.github.com>
2022-07-16 19:48:06 +0530

    Time: 20 ms (98.52%), Space: 13.5 MB (59.95%) - LeetHub

---
+++ 785-is-graph-bipartite/785-is-graph-bipartite.cpp
@@ -0,0 +1,43 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+static auto _ = []() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+  return 0;
+}();
+
+class Solution {
+public:    
+    vector<int>vis;
+    
+    bool recu(int curr,vector<int>&color,int toColor,vector<vector<int>>& graph)
+    {
+        if(color[curr]==-1)color[curr]=toColor;
+        else if(color[curr]!=toColor)return false;
+       
+        if(vis[curr]==1)return true;
+        
+        vis[curr]=1;
+        vector<int>& candid=graph[curr];
+        bool ans=true;
+        for(int i=0;i<candid.size();i++)
+        {            
+            int newColor = (toColor == 0 ? 1 : 0 );
+            ans = ans && recu(candid[i],color,newColor,graph);
+        }
+        return ans;
+    }
+    
+    bool isBipartite(vector<vector<int>>& graph) {
+        vector<int>color(graph.size(),-1);    
+        bool ans=true;
+        vis.resize(graph.size(),0);
+        for(int i=0;i<graph.size();i++)
+        {
+            if(vis[i]==1)continue ;
+            ans=ans && recu(i,color,0,graph);
+        }
+        return ans;
+    }
+};
