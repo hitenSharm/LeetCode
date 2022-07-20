3bf83f6d4bdc23f8f5ab57d67e55992299e66ec2
Hiten Sharma <56029311+hitenSharm@users.noreply.github.com>
2022-07-20 12:26:28 +0530

    Time: 3 ms (91.22%), Space: 11.2 MB (89.85%) - LeetHub

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,32 +1,26 @@
 class Solution {
 public:
-
-    vector<vector<int>>dp;
-
-    int recu(vector<int>& prices,int bs,int index)
-    {
-        if(index>=prices.size())return 0;        
-        int ans=0;
-        if(dp[index][bs]!=-1)return dp[index][bs];
-        if(bs==0)
-        {
-            //can buy
-            int t1=recu(prices,bs,index+1);
-            int t2=recu(prices,1,index+1) - prices[index];
-            ans = max(t1,t2);
-        }
-        else
-        {
-            //can sell
-            int t1=recu(prices,bs,index+1);
-            int t2=recu(prices,0,index+2) + prices[index];
-            ans = max(t1,t2);
-        }
-        return dp[index][bs]=ans;
-    }
-
     int maxProfit(vector<int>& prices) {
-        dp.resize(prices.size(),vector<int>(2,-1));
-        return recu(prices,0,0);
+        //at each point we can either buy or sell
+        // so we can have 2 arrays biy & sell
+        //if i wanna sell at i
+        //sel[i] = max of either i dont sell(sell[i-1]) or the max i 
+        //profit by buying till that moment + p[i] => b[i-1] + p[i]
+        
+        //calc b[i] = max of eithher no buy => b[i-1] or i must have sold something to buy again, (no buy buy    option,invalid) => sell[i-2] - p[i] as sell is on cool down
+        
+        vector<int>buy(prices.size(),0);
+        vector<int>sell(prices.size(),0);
+        
+        buy[0]=-prices[0];
+        for(int i=1;i<prices.size();i++)
+        {
+            if(i-2<0)buy[i]=max(-prices[i],buy[i-1]);
+            else
+            buy[i]=max(sell[i-2]-prices[i],buy[i-1]);
+            
+            sell[i]=max(sell[i-1],buy[i-1]+prices[i]);
+        }
+        return max(buy[prices.size()-1],sell[prices.size()-1]);
     }
 };
