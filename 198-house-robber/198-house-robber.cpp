39e7f96f45ff7110cdac31864543fa34db8a0dee
Hiten Sharma <56029311+hitenSharm@users.noreply.github.com>
2022-07-20 22:33:02 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (47.09%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -18,16 +18,30 @@ public:
     //     int notrobbed=recu(i-1,nums,dp);
     //     return max(robbed,notrobbed);
     // }
+    
+    // void tabular(){
+    //     consider an array of 2 elements to understand
+    //     vector<int>dp(nums.size(),0);
+    //     dp[0]=nums[0];
+    //     dp[1]=max(nums[1],dp[0]);
+    //     for(int i=2;i<nums.size();i++)
+    //     {
+    //         dp[i]=max(dp[i-1],nums[i]+dp[i-2]);
+    //     }
+    //     return dp[nums.size()-1];
+    // }
 
     int rob(vector<int>& nums) {
         if(nums.size()==1)return nums[0];
-        vector<int>dp(nums.size(),0);
-        dp[0]=nums[0];
-        dp[1]=max(nums[1],dp[0]);
+        int curr,prev,prev2;
+        prev2=nums[0];
+        prev=max(nums[1],prev2);        
         for(int i=2;i<nums.size();i++)
         {
-            dp[i]=max(dp[i-1],nums[i]+dp[i-2]);
+            curr=max(prev,nums[i]+prev2);
+            prev2=prev;
+            prev=curr;//sets prev to dp[i]
         }
-        return dp[nums.size()-1];
+        return prev;
     }
 };
