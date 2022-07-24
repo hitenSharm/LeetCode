91da6e170fa934ff01af0df688af4c784b6c58c7
Hiten Sharma <56029311+hitenSharm@users.noreply.github.com>
2022-07-24 11:34:04 +0530

    Time: 34 ms (13.14%), Space: 13.3 MB (52.88%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -1,34 +1,41 @@
 class Solution {
 public:
-    void setZeroes(vector<vector<int>>& matrix) {
-        vector<int>r(matrix.size(),1);
-        vector<int>c(matrix[0].size(),1);
-        for(int i=0;i<matrix.size();i++)
+    void setZeroes(vector<vector<int>>& mat) {
+        //approach 1
+        //if mat elements were just 0,1 make all 0 in matrix -1
+        //then loop again and for each -1 make row,col=0
+        //tc : o(m*n)+(m+n) sc:o(1)
+        //similar to approach 1, if mat elements can be anything
+        //stroe another array marking elements u need to be 0. o(mn) space
+        //approach 2
+        //have 2 sets, iterate, if 0 , add that row and col to sets
+        //iterate again, if either row or col is in set, make mat[i][j]=0
+        //tc: o(m*n) sc:o(m+n)
+        
+        //app 3:
+        //see neetcodes vid,
+        // basically, ik that there will be overlapping at 0,0 if i tyr and use the extra m+n arrays inside the matrix
+        //so to deal with that i create antoehr element which tells the col wether it's supposed to be 0 or not
+        //elements in col 0 check with the extra element if it needs to be turned into a 0 & elements in row 1 check with 0,0
+        bool makeCol=false;
+        for(int i=0;i<mat.size();i++)
         {
-            for(int j=0;j<matrix[0].size();j++)
+            if(mat[i][0]==0)makeCol=true;
+            for(int j=1;j<mat[i].size();j++)
             {
-                if(matrix[i][j]==0)
+                if(mat[i][j]==0)
                 {
-                    r[i]=0;
-                    c[j]=0;
+                    mat[i][0]=0;
+                    mat[0][j]=0;
                 }
             }
-        }
-        for(int i=0;i<r.size();i++)
+        }   
+        for(int i=mat.size()-1;i>=0;i--)
         {
-            if(r[i]==0)
-            for(int j=0;j<matrix[0].size();j++)
-            {
-                matrix[i][j]=0;
-            }
+            for(int j=mat[i].size()-1;j>=1;j--)
+                if(mat[i][0]==0 || mat[0][j]==0)mat[i][j]=0;
+            if(makeCol)mat[i][0]=0;
         }
-        for(int i=0;i<c.size();i++)
-        {
-            if(c[i]==0)
-            for(int j=0;j<matrix.size();j++)
-            {
-                matrix[j][i]=0;
-            }
-        }
+        
     }
 };
