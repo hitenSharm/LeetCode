65419ea07529eb3cf804aa34f191078016e6b89e
Hiten Sharma <56029311+hitenSharm@users.noreply.github.com>
2022-07-24 10:09:06 +0530

    Time: 59 ms (11.14%), Space: 15.2 MB (18.13%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int fl=0;
-    void dfs(vector<int>& vis,unordered_map<int,vector<int>>& adj,int c,vector<int>inDfs)
+    void dfs(vector<int>& vis,unordered_map<int,vector<int>>& adj,int c,vector<int>& inDfs)
     {
         if(fl==1)return ;
         vis[c]=1;
@@ -16,6 +16,7 @@ public:
             if(vis[candi[i]]==1)continue ; 
             dfs(vis,adj,candi[i],inDfs);
         }        
+        inDfs[c]=0;
     }
     
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
@@ -25,10 +26,10 @@ public:
             adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
         }
         vector<int>vis(numCourses,0);
+        vector<int>inDfs(numCourses,0);
         for(int i=0;i<numCourses;i++)
         {
-            if(vis[i]==1)continue ;
-            vector<int>inDfs(numCourses,0);
+            if(vis[i]==1)continue ;            
             dfs(vis,adj,i,inDfs);
         }
         if(fl==1)return false;
