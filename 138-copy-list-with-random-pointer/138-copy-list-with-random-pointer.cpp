a7c142972f00ff81a52df279b17477195e2950be
Hiten Sharma <56029311+hitenSharm@users.noreply.github.com>
2022-07-26 19:40:13 +0530

    Time: 17 ms (33.10%), Space: 11.3 MB (59.85%) - LeetHub

--- 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cpp
@@ -17,38 +17,24 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-        if(!head)return head;
-        Node* th=head;
-        Node* newList=new Node(head->val);
-        Node* newHead=newList;        
-        unordered_map<Node*,int>ump;
-        unordered_map<int,Node*>ump2;
-        int index=0;
-        ump[th]=index;
-        ump2[index]=newList;
-        th=th->next;
-        index++;
-        while(th)
-        {
-            newList->next=new Node(th->val);
-            ump2[index]=newList->next;
-            ump[th]=index;
-            th=th->next;
-            index++;
-            newList=newList->next;
+        unordered_map<Node*,Node*>originalToRandom;
+        unordered_map<Node*,Node*>originalReference;
+        Node* dummy=new Node(-1);
+        Node* newHead=dummy;
+        while(head){
+            Node* temp=new Node(head->val);            
+            dummy->next=temp;
+            originalToRandom[head]=head->random;
+            originalReference[head]=temp;
+            dummy=dummy->next;
+            head=head->next;
         }
-        newList=newHead;
-        th=head;
-        while(th)
+        for(auto it:originalToRandom)
         {
-            if(th->random)
-            {
-                int randomIndexOld=ump[th->random];
-                newList->random=ump2[randomIndexOld];
-            }
-            th=th->next;
-            newList=newList->next;
+            Node* ogNode=it.first;
+            Node* ogRandom=it.second;
+            originalReference[it.first]->random=originalReference[it.second];
         }
-        return newHead;
+        return newHead->next;
     }
 };
