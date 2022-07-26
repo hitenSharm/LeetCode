14bf562d01247c61214830ea79f69ebfb77a0a99
Hiten Sharma <56029311+hitenSharm@users.noreply.github.com>
2022-07-26 16:42:47 +0530

    Time: 8 ms (86.64%), Space: 9.7 MB (57.71%) - LeetHub

--- 256-paint-house/256-paint-house.cpp
+++ 256-paint-house/256-paint-house.cpp
@@ -1,24 +1,31 @@
 class Solution {
 public:
     
-    int recu(vector<vector<int>>& costs,int index,int color,vector<vector<int>>& dp)
-    {
-        if(index>=costs.size())return 0;
-        if(color!=-1 and dp[index][color]!=-1)return dp[index][color];
-        int ans=INT_MAX;
-        for(int i=0;i<3;i++)
-        {
-            if(i==color)continue ;
-            ans=min(ans,costs[index][i] + recu(costs,index+1,i,dp));
-        }
-        if(color==-1)
-        return ans;
+//     int recu(vector<vector<int>>& costs,int index,int color,vector<vector<int>>& dp)
+//     {
+//         if(index>=costs.size())return 0;
+//         if(color!=-1 and dp[index][color]!=-1)return dp[index][color];
+//         int ans=INT_MAX;
+//         for(int i=0;i<3;i++)
+//         {
+//             if(i==color)continue ;
+//             ans=min(ans,costs[index][i] + recu(costs,index+1,i,dp));
+//         }
+//         if(color==-1)
+//         return ans;
         
-        return dp[index][color]=ans;
-    }
+//         return dp[index][color]=ans;
+//     }
     
     int minCost(vector<vector<int>>& costs) {
-        vector<vector<int>>dp(costs.size(),vector<int>(3,-1));
-        return recu(costs,0,-1,dp);
+        
+        for(int i=1;i<costs.size();i++)
+        {
+            costs[i][0]=costs[i][0]+min(costs[i-1][1],costs[i-1][2]);
+            costs[i][1]+=min(costs[i-1][0],costs[i-1][2]);
+            costs[i][2]+=min(costs[i-1][0],costs[i-1][1]);
+        }
+        int n=costs.size()-1;
+        return min(costs[n][0],min(costs[n][1],costs[n][2]));
     }
 };
