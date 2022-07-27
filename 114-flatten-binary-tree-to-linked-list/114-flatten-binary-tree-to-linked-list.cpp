2e9a268426b5efed4a078f468f5d36afd19d3cee
Hiten Sharma <56029311+hitenSharm@users.noreply.github.com>
2022-07-27 09:10:08 +0530

    Time: 7 ms (81.17%), Space: 12.6 MB (78.06%) - LeetHub

--- 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.cpp
+++ 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.cpp
@@ -1,28 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     void flatten(TreeNode* root) {
         if(!root)return ;
         flatten(root->left);
         flatten(root->right);
-        if(root->left and root->right)
-        {
-        	TreeNode* temp=root->right;
-        	root->right=root->left;     
-        	TreeNode* curr=root->right;   	
-        	while(curr->right)
-        	{
-        		curr=curr->right;
-                curr->left=nullptr;
-        	}
-        	curr->right=temp;
-            curr->left=nullptr;
-            root->left=nullptr;
-            return ;
+        TreeNode* temp=root->right;
+        root->right=root->left;
+        root->left=nullptr;
+        TreeNode* iter=root;
+        while(iter and iter->right){
+            iter->left=nullptr;
+            iter=iter->right;            
         }
-        if(root->left)
-        {
-        	root->right=root->left;
-            root->left=nullptr;
-        }
+        if(iter)
+        iter->right=temp;
     }
 };
