44dbc7662aaea382decbac4060e3ab903b166852
Hiten Sharma <56029311+hitenSharm@users.noreply.github.com>
2022-07-28 02:09:03 +0530

    Time: 513 ms (83.51%), Space: 174.1 MB (36.63%) - LeetHub

--- 146-lru-cache/146-lru-cache.cpp
+++ 146-lru-cache/146-lru-cache.cpp
@@ -1,43 +1,42 @@
-class LRUCache {
-public:
-	list<pair<int,int>>cache;
-	unordered_map<int,list<pair<int,int>> :: iterator>hash;
-
-	void makeMostRecent(int key,int value)
-	{
-		cache.erase(hash[key]);
-		cache.push_front({key,value});
-		hash[key]=cache.begin();
-	}
-	int cap;	
+    class LRUCache {
+        
+    private:
+        list<pair<int,int>>cache;
+        unordered_map<int,list<pair<int,int>> :: iterator>ump;
+        int cap;
+    public:
+    
+    void putFront(int k,int v)
+    {        
+        cache.erase(ump[k]);//erase old posi from dll
+        cache.push_front({k,v});//move to front of list
+        ump[k]=cache.begin();//update new value
+    }
+        
     LRUCache(int capacity) {
         cap=capacity;
     }
-     
+    
     int get(int key) {
-        if(hash.find(key)!=hash.end())
-        {
-        	int val=(*hash[key]).second;
-        	makeMostRecent(key,val);
-        	return val;
-        }
-        return -1;
+        if(ump.find(key)==ump.end())return -1;
+        int value=(*ump[key]).second;
+        putFront(key,value);
+        return value;
     }
     
     void put(int key, int value) {
-        if(hash.find(key)!=hash.end())
-        {
-        	makeMostRecent(key,value);
-        }
-        else
-        {
-        	cache.push_front({key,value});
-        	hash[key]=cache.begin();        	
-        	if(hash.size()>cap)
-        	{
-        		hash.erase(cache.back().first);//last element key
-        		cache.pop_back();
-        	}
+        //already in list
+        if(ump.find(key)!=ump.end()){
+            //put in front            
+            putFront(key,value);
+        }else{
+            //not in list
+            cache.push_front({key,value});
+            ump[key]=cache.begin();
+            if(ump.size()>cap){
+                ump.erase(cache.back().first);//erase last element using key
+                cache.pop_back();
+            }
         }
     }
 };
