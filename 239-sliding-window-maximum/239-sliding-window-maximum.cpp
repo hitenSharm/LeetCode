5791f70343caeb9584835c1bf5bb44df30a2979a
Hiten Sharma <56029311+hitenSharm@users.noreply.github.com>
2022-07-29 16:10:43 +0530

    Time: 484 ms (46.85%), Space: 134.7 MB (57.36%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/239-sliding-window-maximum.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        //maintiani a deque of max size = k and monotonically decreasing
         deque<int>dq;
         vector<int>ans;
-        for(int i=0;i<nums.size();i++)
-        {
-            if(!dq.empty() and dq.front() <= i-k)dq.pop_front();
-            //out of bounds check
-            while(!dq.empty() and nums[dq.back()]<=nums[i])
-            {
+        for(int i=0;i<nums.size();i++){
+            if(!dq.empty() and dq.front()<i-k+1)dq.pop_front();
+            while(!dq.empty() and nums[dq.back()]<nums[i]){
                 dq.pop_back();
-            }
-            dq.push_back(i);
+            }            
+            dq.push_back(i);            
             if(i>=k-1)ans.push_back(nums[dq.front()]);
-        }
+        }        
         return ans;
     }
 };
