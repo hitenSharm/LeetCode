7d4da939b9f55908bfc00cab110e3551e54a26f7
Hiten Sharma <56029311+hitenSharm@users.noreply.github.com>
2022-07-30 16:41:41 +0530

    Time: 21 ms (79.19%), Space: 16.6 MB (53.65%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.cpp
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
-
-    vector<int>dp;
-    int recu(int i,vector<int>& nums)
-    {
-        if(i>=nums.size()-1)return 0;
-        if(dp[i]!=-1)return dp[i];
-        int ans=100001;
-        for(int j=1;j<=nums[i];++j)
-        {
-            ans=min(ans,1+recu(i+j,nums));
-        }
-        return dp[i]=ans;
-    }
-
     int jump(vector<int>& nums) {
-        dp.resize(nums.size(),-1);
-        return recu(0,nums);
+        int ans=0;
+        int farthestEnd=0;
+        int endHere=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            int canTravel=i+nums[i];
+            farthestEnd=max(farthestEnd,canTravel);
+            if(i==endHere)
+            {
+                if(i!=nums.size()-1)
+                ans++;
+                endHere=farthestEnd;
+            }
+        }
+        return ans;
     }
 };
