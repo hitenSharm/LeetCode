9c7f59fb06aa6ab7fd0239b35ed31a729f374ce0
Hiten Sharma <56029311+hitenSharm@users.noreply.github.com>
2022-07-31 16:05:14 +0530

    Time: 290 ms (21.13%), Space: 112.8 MB (11.97%) - LeetHub

--- 1746-maximum-subarray-sum-after-one-operation/1746-maximum-subarray-sum-after-one-operation.cpp
+++ 1746-maximum-subarray-sum-after-one-operation/1746-maximum-subarray-sum-after-one-operation.cpp
@@ -5,19 +5,15 @@ public:
         vector<vector<int>>dp(nums.size(),vector<int>(2,0));
         dp[0][0]=nums[0];
         dp[0][1]=nums[0]*nums[0]; //square at that index
-        int ans=0;
+        int ans=max(dp[0][0],dp[0][1]);
         for(int i=1;i<nums.size();i++)
         {
             if(dp[i-1][0]+nums[i]<0)dp[i][0]=0;
             else
                 dp[i][0]=dp[i-1][0]+nums[i];
             dp[i][1]=max(dp[i-1][0]+(nums[i]*nums[i]),dp[i-1][1] + nums[i]);            
-        }
-        for(int i=0;i<dp.size();i++)
-            ans=max(ans,max(dp[i][0],dp[i][1]));
-        // cout<<endl;
-        // for(int i=0;i<dp.size();i++)
-        //     cout<<dp[i][1]<<" ";
+            ans=max(ans,max(dp[i][0],dp[i][1]));  
+        }              
         return ans;
     }
 };
