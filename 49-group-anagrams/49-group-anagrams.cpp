c060e022fcbb5a757e764459f8a298456d422c3b
Hiten Sharma <56029311+hitenSharm@users.noreply.github.com>
2022-07-31 13:52:32 +0530

    Time: 48 ms (72.78%), Space: 22.1 MB (19.70%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.cpp
+++ 49-group-anagrams/49-group-anagrams.cpp
@@ -1,19 +1,41 @@
 class Solution {
 public:
-    vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        map<vector<int>,vector<string>>mp;
-        for(auto i:strs)
+    vector<vector<string>>ans;
+    
+    void solver(vector<string>& strs)
+    {
+        unordered_map<string,vector<string>>ump;
+        for(int i=0;i<strs.size();i++)
         {
-            vector<int>freq(26,0);
-            for(int j=0;j<i.size();j++)
-            {
-               freq[i[j]-'a']++;
-            }
-            mp[freq].push_back(i);
+            string temp=strs[i];
+            sort(temp.begin(),temp.end());
+            ump[temp].push_back(strs[i]);
         }
-        vector<vector<string>>ans;
-        for(auto it:mp)
+        for(auto it:ump)
+        {
             ans.push_back(it.second);
+        }
+    }
+    
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        solver(strs);
         return ans;
+        // map<vector<int>,vector<string>>mp;
+        // //or make an unordered map in which u make a copy of string at each iteration and push the string in unorderred map of string and vector based on the sorted copy string
+        // //
+        // for(auto i:strs)
+        // {
+        //     vector<int>freq(26,0);
+        //     for(int j=0;j<i.size();j++)
+        //     {
+        //        freq[i[j]-'a']++;
+        //     }
+        //     mp[freq].push_back(i);
+        // }
+        // vector<vector<string>>ans;
+        // for(auto it:mp)
+        //     ans.push_back(it.second);
+        // return ans;
+        // SC : o(nk)  as k is the size of string and n is array length
     }
 };
