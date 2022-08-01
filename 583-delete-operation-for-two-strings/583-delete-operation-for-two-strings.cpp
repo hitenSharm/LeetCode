6b8d15d5a170e173750de14e7959a6d448c537a6
Hiten Sharma <56029311+hitenSharm@users.noreply.github.com>
2022-08-01 09:26:59 +0530

    Time: 33 ms (51.74%), Space: 7.6 MB (72.03%) - LeetHub

--- 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
@@ -1,17 +1,42 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+static auto _ = []() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+  return 0;
+}();
 class Solution {
 public:
-    int dp[501][501];
+    
     int recu(string& s1,string& s2,int i,int j)
     {
-        if(i==s1.size() || j==s2.size())return ((s1.size()-i)+(s2.size()-j));
-        if(dp[i][j]!=-1)return dp[i][j];
-        if(s1[i]==s2[j])return recu(s1,s2,i+1,j+1);
+        if(i==0 || j==0)return i+j;
         
-        return dp[i][j]=min((1+recu(s1,s2,i+1,j)),(1+recu(s1,s2,i,j+1)));
+        if(s1[i-1]==s2[j-1])return recu(s1,s2,i-1,j-1);
+        
+        return min((1+recu(s1,s2,i-1,j)),(1+recu(s1,s2,i,j-1)));
     }
     
-    int minDistance(string word1, string word2) {
-        memset(dp,-1,sizeof(dp));
-        return recu(word1,word2,0,0);
+    int minDistance(string word1, string word2) {      
+        int dp[501][501];
+        
+        for(int i=0;i<=word1.size();i++)dp[i][0]=i;
+        for(int j=0;j<=word2.size();j++)dp[0][j]=j;
+        
+        for(int i=1;i<=word1.size();i++)
+        {
+            for(int j=1;j<=word2.size();j++)
+            {
+                if(word1[i-1]==word2[j-1])dp[i][j]=dp[i-1][j-1];
+                else
+                {
+                    dp[i][j]=1+min(dp[i-1][j],dp[i][j-1]);
+                }                
+            }
+        }
+        
+        return dp[word1.size()][word2.size()];
+                
     }
 };
