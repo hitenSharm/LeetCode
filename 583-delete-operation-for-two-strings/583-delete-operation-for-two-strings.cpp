2996dd272ff6a5bd1c58d0ec6e70b1f177334c87
Hiten Sharma <56029311+hitenSharm@users.noreply.github.com>
2022-08-01 09:27:08 +0530

    Time: 52 ms (26.73%), Space: 7.4 MB (90.96%) - LeetHub

--- 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
@@ -9,14 +9,14 @@ static auto _ = []() {
 class Solution {
 public:
     
-    int recu(string& s1,string& s2,int i,int j)
-    {
-        if(i==0 || j==0)return i+j;
+//     int recu(string& s1,string& s2,int i,int j)
+//     {
+//         if(i==0 || j==0)return i+j;
         
-        if(s1[i-1]==s2[j-1])return recu(s1,s2,i-1,j-1);
+//         if(s1[i-1]==s2[j-1])return recu(s1,s2,i-1,j-1);
         
-        return min((1+recu(s1,s2,i-1,j)),(1+recu(s1,s2,i,j-1)));
-    }
+//         return min((1+recu(s1,s2,i-1,j)),(1+recu(s1,s2,i,j-1)));
+//     }
     
     int minDistance(string word1, string word2) {      
         int dp[501][501];
