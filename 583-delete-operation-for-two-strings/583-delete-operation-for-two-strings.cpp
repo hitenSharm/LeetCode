595158178246fcc20a5625933df39cc570428ed6
Hiten Sharma <56029311+hitenSharm@users.noreply.github.com>
2022-08-01 14:20:53 +0530

    Time: 23 ms (70.69%), Space: 6.9 MB (95.93%) - LeetHub

--- 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
@@ -19,24 +19,31 @@ public:
 //     }
     
     int minDistance(string word1, string word2) {      
-        int dp[501][501];
+        // int dp[501][501];
         
-        for(int i=0;i<=word1.size();i++)dp[i][0]=i;
-        for(int j=0;j<=word2.size();j++)dp[0][j]=j;
+        // for(int i=0;i<=word1.size();i++)dp[i][0]=i; //1 based indexing
+        // for(int j=0;j<=word2.size();j++)dp[0][j]=j;
+        
+        vector<int>prev(word2.size()+1,0);
+        for(int j=0;j<=word2.size();j++)prev[j]=j;
+        vector<int>curr(word2.size()+1,0);
+        
         
         for(int i=1;i<=word1.size();i++)
         {
+            curr[0]=i;
             for(int j=1;j<=word2.size();j++)
             {
-                if(word1[i-1]==word2[j-1])dp[i][j]=dp[i-1][j-1];
+                if(word1[i-1]==word2[j-1])curr[j]=prev[j-1];
                 else
                 {
-                    dp[i][j]=1+min(dp[i-1][j],dp[i][j-1]);
+                    curr[j]=1+min(prev[j],curr[j-1]);
                 }                
             }
+            prev=curr;
         }
         
-        return dp[word1.size()][word2.size()];
+        return prev[word2.size()];
                 
     }
 };
