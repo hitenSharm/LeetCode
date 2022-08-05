a0e0531e115f0c896d148fd98a27e0b1fcf2ad97
Hiten Sharma <56029311+hitenSharm@users.noreply.github.com>
2022-08-05 16:26:24 +0530

    Time: 553 ms (30.37%), Space: 10.7 MB (23.51%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,36 +1,26 @@
-#pragma GCC optimize("Ofast")
-#pragma GCC target("avx,avx2,fma")
-static auto _ = []() {
-  ios_base::sync_with_stdio(false);
-  cin.tie(nullptr);
-  cout.tie(nullptr);
-  return 0;
-}();
-
 class Solution {
 public:
-	int dp[2501];
-	int recu(vector<int>& nums,int i)
-	{
-		if(i>=nums.size())return 0;
-		if(dp[i]!=-1)return dp[i];
-		int maxi=0;
-		for(int j=i+1;j<nums.size();j++)
-		{
-			if(nums[j]>nums[i])
-			{
-				maxi=max(maxi,1+recu(nums,j));
-			}
-		}
-		return dp[i]=maxi;
-	}
-
-    int lengthOfLIS(vector<int>& nums) {		
-		memset(dp,-1,sizeof(dp));
-        int len=1;
-        for(int i=0;i<nums.size();++i){
-            len=max(len,1+recu(nums,i));
+    vector<int>dp;
+    int recu(vector<int>& nums,int i)
+    {
+        if(i>=nums.size())return 0;
+        int ans=1;
+        if(dp[i]!=-1)return dp[i];
+        for(int j=i+1;j<nums.size();j++)
+        {
+            if(nums[j]>nums[i])
+            ans=max(ans,1+recu(nums,j));
         }
-		return len;
+        return dp[i]=ans;
+    }
+    
+    int lengthOfLIS(vector<int>& nums) {
+        int ans=0;
+        dp.resize(nums.size(),-1);
+        for(int i=0;i<nums.size();i++)
+        {
+            ans=max(ans,recu(nums,i));            
+        }
+        return ans;
     }
 };
