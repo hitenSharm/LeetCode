a4ae8da0a8d9cb124fa3bac81c4acf6e91e48f66
Hiten Sharma <56029311+hitenSharm@users.noreply.github.com>
2022-08-05 18:30:24 +0530

    Time: 126 ms (62.93%), Space: 15.4 MB (40.49%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,24 +1,28 @@
 class Solution {
 public:
-	long long dp[10001];
-	long long recu(long long amt,vector<int>& coins, int& amount)
-	{
-		if(amt>amount)return 100000;
-		if(amt==amount)return 0;	
-		if(dp[amt]!=-1)return dp[amt];	
-		long long ans=INT_MAX;
-		for(int i=0;i<coins.size();i++)
-		{
-			ans=min(ans,1+recu(amt+coins[i],coins,amount));
-		}
-		return dp[amt]=ans;
-	}
-
+    
+    vector<int>dp;
+    
+    int recu(int amt,vector<int>& c)
+    {
+        if(amt==0)return 0;
+        if(amt<0)return -1;        
+        int ans=INT_MAX;
+        if(dp[amt]!=-2)return dp[amt];
+        for(int i=0;i<c.size();i++)
+        {
+            int temp=recu(amt-c[i],c);
+            if(temp!=-1)ans=min(ans,1+temp);
+        }
+        if(ans==INT_MAX)ans=-1;
+        return dp[amt]=ans;
+    }
+    
+    
     int coinChange(vector<int>& coins, int amount) {
-		memset(dp,-1,sizeof(dp));
-        long long ans=recu(0,coins,amount);
-        if(ans>=100000)
-            ans=-1;
-        return ans;
+        dp.resize(amount+1,-2);
+        
+        return recu(amount,coins);
+        
     }
 };
