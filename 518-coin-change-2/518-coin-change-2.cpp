1c494245d9a66812a0a434654ce8f3d76c545751
Hiten Sharma <56029311+hitenSharm@users.noreply.github.com>
2022-08-05 11:11:20 +0530

    Time: 52 ms (39.59%), Space: 21.8 MB (7.25%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.cpp
+++ 518-coin-change-2/518-coin-change-2.cpp
@@ -3,8 +3,8 @@ public:
     vector<vector<int>>dp;
     int recu(int amt,vector<int>& c,int index)
     {
-        if(index>=c.size() || amt<0)return 0;
         if(amt==0)return 1;
+        if(index>=c.size() || amt<0)return 0;        
         if(dp[amt][index]!=-1)return dp[amt][index];
         
         return dp[amt][index]=recu(amt-c[index],c,index)+recu(amt,c,index+1);
