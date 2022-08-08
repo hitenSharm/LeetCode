8783be1fa233416f5cb5a3e9e8a62856aba9aad6
Hiten Sharma <56029311+hitenSharm@users.noreply.github.com>
2022-08-08 11:38:23 +0530

    Time: 376 ms (55.52%), Space: 10.6 MB (28.08%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,19 +1,32 @@
 class Solution {
 public:
-    vector<vector<int>>dp;
-    int recu(vector<int>& nums, int prev, int index)
-    {
-        if (index >= nums.size())return 0;
-        if (dp[prev + 1][index] != -1)return dp[prev + 1][index];
+    //vector<vector<int>>dp;
+    //     int recu(vector<int>& nums, int prev, int index)
+    //     {
+    //         if (index >= nums.size())return 0;
+    //         if (dp[prev + 1][index] != -1)return dp[prev + 1][index];
 
-        int ans = recu(nums, prev, index + 1);//dont take that index or cant take that index        
-        if (prev == -1 || nums[index] > nums[prev])
-            ans = max(ans, 1 + recu(nums, index, index + 1));
-        return dp[prev+1][index]=ans;
-    }
+    //         int ans = recu(nums, prev, index + 1);//dont take that index or cant take that index        
+    //         if (prev == -1 || nums[index] > nums[prev])
+    //             ans = max(ans, 1 + recu(nums, index, index + 1));
+    //         return dp[prev+1][index]=ans;
+    //     }
 
     int lengthOfLIS(vector<int>& nums) {
-        dp.resize(nums.size()+1, vector<int>(nums.size(), -1));
-        return recu(nums, -1, 0);
+        vector<int>lis(nums.size(), 0);
+
+        lis[0] = 1;//lis till that index
+
+        for (int i = 1; i < nums.size(); i++)
+        {
+            lis[i]=1;
+            for (int j = 0; j < i; j++)
+            {
+                if (nums[i] > nums[j] and lis[i] < lis[j] + 1)
+                    lis[i] = lis[j] + 1;
+            }
+        }
+        return *max_element(lis.begin(),lis.end());
+        //do nlog n
     }
 };
