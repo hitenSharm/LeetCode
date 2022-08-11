5e4f9e6931c1d4c652e2125c447eb61be49b0347
Hiten Sharma <56029311+hitenSharm@users.noreply.github.com>
2022-08-11 23:38:19 +0530

    Time: 48 ms (32.75%), Space: 14.6 MB (63.41%) - LeetHub

---
+++ 332-reconstruct-itinerary/332-reconstruct-itinerary.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+
+    unordered_map<string, multiset<string>>adj;    
+    vector<string>ans;
+
+    void recu(string curr)
+    {
+        while(adj[curr].size()>0)
+        {
+            string next = *adj[curr].begin();
+            adj[curr].erase(adj[curr].begin());
+            recu(next);
+        }
+        ans.push_back(curr);
+    }
+
+    vector<string> findItinerary(vector<vector<string>>& tickets) {
+        for (int i = 0; i < tickets.size(); i++)
+        {
+            adj[tickets[i][0]].insert(tickets[i][1]);
+        }
+
+        recu("JFK");
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
