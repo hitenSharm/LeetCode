a43ca1c2cd500c1616b75be7b281e1ee051c5e4a
Hiten Sharma <56029311+hitenSharm@users.noreply.github.com>
2022-08-18 10:25:21 +0530

    Time: 7 ms (53.97%), Space: 11.2 MB (29.48%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -1,29 +1,45 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
+    
+    int findPivot(vector<int>& nums)
+    {
+        if(nums.size()==1)return 0;
+        //if(nums[0]>nums[1])return 0;
         int l=0;
         int r=nums.size()-1;
         while(l<=r)
         {
-            int m=l+(r-l)/2;
-            if(nums[m]==target)return m;
-            if(nums[l]<=nums[m])
+            int m = l + (r-l)/2;            
+            if(nums[m]>nums[m+1])return m;
+            else if(nums[m]>nums[nums.size()-1])
             {
-                //first sorted range
-                if(target<nums[m] and target>=nums[l]){
-                    r=m-1;
-                }
-                else
-                {
-                    l=m+1;
-                }
-            }else{
-                if(target>nums[m] and target<=nums[r]){
-                    l=m+1;
-                }else
-                    r=m-1;
+                l=m+1;
             }
+            else
+                r=m-1;
         }
         return -1;
+    }
+    
+    int bs(vector<int>& nums,int& t,int l,int r)
+    {        
+        while(l<=r)
+        {
+            int m = l+ (r-l)/2;
+            if(nums[m]==t)return m;
+            if(nums[m]<t)l=m+1;
+            else
+                r=m-1;
+        }
+        return -1;
+    }
+    
+    int search(vector<int>& nums, int target) {
+        if(nums.size()==1)return nums[0]==target ? 0 : -1;        
+        int idx=findPivot(nums);
+        cout<<idx<<endl;
+        int ans=bs(nums,target,0,idx);
+        ans=max(ans,bs(nums,target,idx+1,nums.size()-1));
+        return ans;
     }
 };
