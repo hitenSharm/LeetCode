1ebfd8a0e4e71a9e38b4fced2eb64d13804daec2
Hiten Sharma <56029311+hitenSharm@users.noreply.github.com>
2022-08-21 23:31:55 +0530

    Time: 38 ms (54.14%), Space: 14.7 MB (58.24%) - LeetHub

--- 332-reconstruct-itinerary/332-reconstruct-itinerary.cpp
+++ 332-reconstruct-itinerary/332-reconstruct-itinerary.cpp
@@ -1,28 +1,29 @@
 class Solution {
 public:
-
-    unordered_map<string, multiset<string>>adj;    
-    vector<string>ans;
-
-    void recu(string curr)
-    {
-        while(adj[curr].size()>0)
+	unordered_map<string,multiset<string>>adj;
+	vector<string>ans;
+    
+    void recu(string curr){
+        
+        while(adj[curr].size()>0)//imp method to get next here as at times it becomes 0
         {
-            string next = *adj[curr].begin();
+            string next=*adj[curr].begin();
             adj[curr].erase(adj[curr].begin());
             recu(next);
         }
+        
         ans.push_back(curr);
     }
-
+    
     vector<string> findItinerary(vector<vector<string>>& tickets) {
-        for (int i = 0; i < tickets.size(); i++)
+        for(int i=0;i<tickets.size();i++)
         {
-            adj[tickets[i][0]].insert(tickets[i][1]);
+            adj[tickets[i][0]].insert(tickets[i][1]);//make graph and lexo sorted
         }
-
+        
         recu("JFK");
-        reverse(ans.begin(), ans.end());
+	    
+        reverse(ans.begin(),ans.end());
         return ans;
     }
 };
