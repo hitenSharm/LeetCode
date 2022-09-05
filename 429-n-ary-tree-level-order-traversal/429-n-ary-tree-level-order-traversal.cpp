8b4710a21d24559f553fb3278a5f987b4141680e
Hiten Sharma <56029311+hitenSharm@users.noreply.github.com>
2022-09-05 12:47:42 +0530

    Time: 60 ms (5.44%), Space: 11.9 MB (13.52%) - LeetHub

---
+++ 429-n-ary-tree-level-order-traversal/429-n-ary-tree-level-order-traversal.cpp
@@ -0,0 +1,35 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+static auto _ = []() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+  return 0;
+}();
+
+class Solution {
+public:
+    vector<vector<int>> levelOrder(Node* root) {
+        queue<Node*>q;
+        if(!root)return {};
+        q.push(root);
+        vector<vector<int>>ans;
+        while(!q.empty())
+        {
+            int len=q.size();
+            vector<int>temp;
+            for(int i=0;i<len;i++)
+            {
+                Node* tmp=q.front();q.pop();
+                temp.push_back(tmp->val);
+                vector<Node*>& kids=tmp->children;
+                for(int j=0;j<kids.size();j++)
+                {
+                    q.push(kids[j]);
+                }
+            }
+            ans.push_back(temp);
+        }
+        return ans;
+    }
+};
