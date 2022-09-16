62a1a45783d04de5ab9b0b967de837bc20e9cdfa
Hiten Sharma <56029311+hitenSharm@users.noreply.github.com>
2022-09-16 20:40:24 +0530

    Time: 28 ms (28.36%), Space: 10.2 MB (41.17%) - LeetHub

--- 130-surrounded-regions/130-surrounded-regions.cpp
+++ 130-surrounded-regions/130-surrounded-regions.cpp
@@ -1,46 +1,41 @@
-#pragma GCC optimize("Ofast")
-#pragma GCC target("avx,avx2,fma")
-static auto _ = []() {
-  ios_base::sync_with_stdio(false);
-  cin.tie(nullptr);
-  cout.tie(nullptr);
-  return 0;
-}();
-
 class Solution {
 public:
     
-    int dirs[5]={1,0,-1,0,1};
-    
-    void recu(vector<vector<char>>& board,int i,int j)
+    void dfs(vector<vector<char>>& board,int i,int j)
     {
-        if(i<0 || j<0 || i>=board.size() || j>=board[0].size() || board[i][j]!='O')return;
-        board[i][j]='V';
+        int dirs[5]={-1,0,1,0,-1};
+        board[i][j]='@';
         for(int k=0;k<4;k++)
-        {            
-            recu(board,i+dirs[k],j+dirs[k+1]);
+        {
+            int ni=i+dirs[k],nj=j+dirs[k+1];
+            if(ni>=0 and nj>=0 and ni<board.size() and nj<board[0].size() and board[ni][nj]=='O')
+                dfs(board,ni,nj);
         }
     }
     
     void solve(vector<vector<char>>& board) {
+        //start from the edges instead of the normal
         for(int i=0;i<board.size();i++)
         {
-            if(board[i][0]=='O')recu(board,i,0);
-            if(board[i][board[0].size()-1]=='O')recu(board,i,board[0].size()-1);
+            if(board[i][0]=='O')
+                dfs(board,i,0);
+            if(board[i][board[0].size()-1]=='O')
+                dfs(board,i,board[0].size()-1);
         }
-        for(int j=0;j<board[0].size();j++)
+        for(int j=0;j<board[0].size()-1;j++)
         {
-            if(board[0][j]=='O')recu(board,0,j);
-            if(board[board.size()-1][j]=='O')recu(board,board.size()-1,j);
+            if(board[0][j]=='O')
+                dfs(board,0,j);
+            if(board[board.size()-1][j]=='O')
+                dfs(board,board.size()-1,j);
         }
         for(int i=0;i<board.size();i++)
         {
             for(int j=0;j<board[0].size();j++)
             {
-                if(board[i][j]=='V')
-                board[i][j]='O';
+                if(board[i][j]=='@')board[i][j]='O';
                 else
-                board[i][j]='X';
+                    board[i][j]='X';
             }
         }
     }
