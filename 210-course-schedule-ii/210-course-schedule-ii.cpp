d3d061f70550a5b15c9e32ecdb7f020433f18d31
Hiten Sharma <56029311+hitenSharm@users.noreply.github.com>
2022-09-17 15:54:53 +0530

    Time: 56 ms (14.54%), Space: 14.1 MB (47.91%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.cpp
+++ 210-course-schedule-ii/210-course-schedule-ii.cpp
@@ -1,55 +1,44 @@
 class Solution {
 public:
-	stack<int>st;	
-	unordered_set<int>traversedNodes;
-	bool valid=true;
-
-	void recu(int curr,unordered_map<int,vector<int>>& adjList,vector<int>& vis)
-	{		
-		if(!valid)return ;
-		vis[curr]=1;
-		traversedNodes.insert(curr);
-		vector<int>& candid=adjList[curr];
-		for(int i=0;i<candid.size();i++)
-		{			
-			if(vis[candid[i]]!=0)
-			{
-				valid=false;
-				return ;
-                //this should be doing cycle detection by dfs
-                //this is like keeping track of recursion stack
-                //a better apporach is to pass by address the vis array
-                //make vis[current]=1;
-                //then traverse
-                //make vis[current]=0; ---> make move,explore,unmake move
-			}
-			if(traversedNodes.find(candid[i])!=traversedNodes.end())continue ;
-			recu(candid[i],adjList,vis);            
-		}
-        vis[curr]=0;
-		st.push(curr);
-	}
-
-    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        unordered_map<int,vector<int>>adjList;    	
-
-        for(int i=0;i<prerequisites.size();i++)
-        	adjList[prerequisites[i][0]].push_back(prerequisites[i][1]);
-        
-        vector<int>vis(numCourses,0);                
-        for(int i=0;i<numCourses;i++)
+    
+    vector<int>indegree;
+    vector<int>ans;
+    unordered_map<int,vector<int>>adj;
+    void kahns()
+    {
+        queue<int>q;
+        for(int i=0;i<indegree.size();i++)
         {
-        	if(traversedNodes.find(i)!=traversedNodes.end())continue ;        	
-        	recu(i,adjList,vis);
-        	if(!valid)return {};
-        }        
-        vector<int>ans;
-        while(!st.empty())
-        {
-        	ans.push_back(st.top());
-        	st.pop();
+            if(indegree[i]==0)q.push(i);
         }
-        reverse(ans.begin(),ans.end());
+        while(!q.empty())
+        {
+            int curr=q.front();
+            q.pop();
+            ans.push_back(curr);
+            vector<int>& candid=adj[curr];
+            for(auto i:candid)
+            {
+                if(indegree[i]>0)
+                {
+                    indegree[i]--;
+                    if(indegree[i]==0)q.push(i);
+                }
+            }
+        }
+    }
+    
+    
+    vector<int> findOrder(int numCourses, vector<vector<int>>& pre) {
+        //kahns algorithm is better here, other option is topo with cycle dtection using dfs  
+        indegree.resize(numCourses);
+        for(int i=0;i<pre.size();i++)
+        {
+            indegree[pre[i][0]]++;
+            adj[pre[i][1]].push_back(pre[i][0]);
+        }
+        kahns();
+        if(ans.size()!=numCourses)ans.clear();
         return ans;
     }
 };
