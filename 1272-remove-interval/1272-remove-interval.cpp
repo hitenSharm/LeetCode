c5a22278bf44d3e5d75929d999d2d9c2b5ed1d01
Hiten Sharma <56029311+hitenSharm@users.noreply.github.com>
2022-09-22 17:00:47 +0530

    Time: 234 ms (14.78%), Space: 38.3 MB (71.30%) - LeetHub

--- 1272-remove-interval/1272-remove-interval.cpp
+++ 1272-remove-interval/1272-remove-interval.cpp
@@ -2,48 +2,35 @@ class Solution {
 public:
     vector<vector<int>> removeInterval(vector<vector<int>>& intervals, vector<int>& tbr) {
         vector<vector<int>> ans;
-        
         for(int i=0;i<intervals.size();i++)
         {
             int s=intervals[i][0];
             int e=intervals[i][1];
-            if(s==tbr[0] and e>tbr[1])
-            {
-                int ts=tbr[1];
-                int te=e;
-                ans.push_back({ts,te});
-            }
-            else if(s<tbr[0] and e>tbr[1])
-            {
-                int ts=s;
-                int te=tbr[0];
-                ans.push_back({ts,te});
-                ts=tbr[1];
-                te=e;
-                ans.push_back({ts,te});
-            }
-            else if(e==tbr[1] and s<tbr[0])
-            {
-                int ts=s;
-                int te=tbr[0];
-                ans.push_back({ts,te});
-            }
-            else if(e<tbr[0] || s>tbr[1])
+            
+            if(e<tbr[0] || s>tbr[1])
             {
                 ans.push_back({s,e});
             }
-            else if(s<=tbr[1] and e>tbr[1])
+            else
             {
-                int ts=tbr[1];
-                int te=e;
-                ans.push_back({ts,te});
+                if(s<tbr[0])
+                {
+                    ans.push_back({s,tbr[0]});
+                    //s *********************** e
+                    //      @@@@@@@@@@@@@@
+                    //  @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+                    //     @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+                }
+                if(e>tbr[1])
+                {
+                    ans.push_back({tbr[1],e});
+                    // s ************************ e
+                    // @@@@@@@@@@@@@@@@@@@@
+                    //      @@@@@
+                    //  @@@@@@@@@@@@@@@@@@@@@@@@@
+                }
             }
-            else if(s<tbr[0] and e>=tbr[0])
-            {
-                int ts=s;
-                int te=tbr[0];
-                ans.push_back({ts,te});
-            }
+            
         }
         return ans;
     }
