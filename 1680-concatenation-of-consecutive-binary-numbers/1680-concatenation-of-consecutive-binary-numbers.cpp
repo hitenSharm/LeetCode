eedec60042248bf5b31539c2e633ae64629cf6f1
Hiten Sharma <56029311+hitenSharm@users.noreply.github.com>
2022-09-24 09:34:00 +0530

    Time: 37 ms (89.84%), Space: 5.9 MB (82.89%) - LeetHub

--- 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.cpp
+++ 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.cpp
@@ -3,9 +3,12 @@ public:
     int concatenatedBinary(int n) {
         long long ans=1;
         int mod=1e9+7;
+        //tc: o(nlogn)
+        //to check for power of 2 : i & (i-1) == 0
+        int len=1;
         for(int i=2;i<=n;i++)
         {
-            int len=log2(i)+1;
+            if((i & (i-1))==0)len++; //the bits to shift increases at each power of 2
             ans=((ans << len) + i)%mod; //left shift by the new bits that will come and add
             //i as the come to the units side
         }
