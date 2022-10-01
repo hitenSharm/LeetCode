d6e22c85b365143f6f25fa0585541701618d369c
Hiten Sharma <56029311+hitenSharm@users.noreply.github.com>
2022-10-01 11:29:39 +0530

    Time: 12 ms (8.46%), Space: 6.8 MB (25.36%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
 
     unordered_set<string>valids;
-    int dp[101];
+    // int dp[101];
     // int recu(string& s, int i)
     // {
     //     if (i == 0)return 1;
@@ -28,19 +28,21 @@ public:
     // }
 
     int numDecodings(string s) {
-        memset(dp, -1, sizeof(dp));
+        // memset(dp, -1, sizeof(dp));
         for (int i = 1; i <= 26; i++)
         {
             valids.insert(to_string(i));
         }
-        dp[0] = 1;
+        int curr=0;
+        int prev1 = 1;
+        int prev2 = 0;
         for (int i = 1; i <= s.size(); i++)
         {
             string s1 = "";
             s1 += s[i - 1];
             int t1 = 0;
             if (valids.find(s1) != valids.end()) {
-                t1 = dp[i-1];
+                t1 = prev1;
             }
             int t2 = 0;
             if (i - 2 >= 0) {
@@ -49,11 +51,14 @@ public:
                 s2 += s[i - 1];
                 if (valids.find(s2) != valids.end())
                 {
-                    t2 = dp[i-2];
+                    t2 = prev2;
                 }
             }
-            dp[i] = t1+t2;
+            curr = t1+t2;
+            int temp = prev1;
+            prev1 = curr;
+            prev2 = temp;
         }
-        return dp[s.size()];
+        return curr;
     }
 };
