532701918681851ef01c75b999fa2183c39c2729
Hiten Sharma <56029311+hitenSharm@users.noreply.github.com>
2022-10-01 11:26:52 +0530

    Time: 14 ms (6.59%), Space: 6.8 MB (24.73%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -2,30 +2,30 @@ class Solution {
 public:
 
     unordered_set<string>valids;
-    int dp[100];
-    int recu(string& s, int i)
-    {
-        if (i >= s.size())return 1;
-        if (dp[i] != -1)return dp[i];
-        //take one
-        string s1 = "";
-        s1 += s[i];
-        int t1 = 0;
-        if (valids.find(s1) != valids.end()) {
-            t1 = recu(s, i + 1);
-        }
-        int t2 = 0;
-        if (i + 1 < s.size()) {
-            string s2 = "";
-            s2 += s[i];
-            s2 += s[i + 1];
-            if (valids.find(s2) != valids.end())
-            {
-                t2 = recu(s, i + 2);
-            }
-        }
-        return dp[i]=t1 + t2;
-    }
+    int dp[101];
+    // int recu(string& s, int i)
+    // {
+    //     if (i == 0)return 1;
+    //     if (dp[i] != -1)return dp[i];
+    //     //take one
+    //     string s1 = "";
+    //     s1 += s[i - 1];
+    //     int t1 = 0;
+    //     if (valids.find(s1) != valids.end()) {
+    //         t1 = recu(s, i - 1);
+    //     }
+    //     int t2 = 0;
+    //     if (i - 2 >= 0) {
+    //         string s2 = "";
+    //         s2 += s[i - 2];
+    //         s2 += s[i - 1];
+    //         if (valids.find(s2) != valids.end())
+    //         {
+    //             t2 = recu(s, i - 2);
+    //         }
+    //     }
+    //     return dp[i] = t1 + t2;
+    // }
 
     int numDecodings(string s) {
         memset(dp, -1, sizeof(dp));
@@ -33,6 +33,27 @@ public:
         {
             valids.insert(to_string(i));
         }
-        return recu(s, 0);
+        dp[0] = 1;
+        for (int i = 1; i <= s.size(); i++)
+        {
+            string s1 = "";
+            s1 += s[i - 1];
+            int t1 = 0;
+            if (valids.find(s1) != valids.end()) {
+                t1 = dp[i-1];
+            }
+            int t2 = 0;
+            if (i - 2 >= 0) {
+                string s2 = "";
+                s2 += s[i - 2];
+                s2 += s[i - 1];
+                if (valids.find(s2) != valids.end())
+                {
+                    t2 = dp[i-2];
+                }
+            }
+            dp[i] = t1+t2;
+        }
+        return dp[s.size()];
     }
 };
