9341e61567716a35d3033d25828baa8b3aa66ee1
Hiten Sharma <56029311+hitenSharm@users.noreply.github.com>
2022-10-08 22:15:07 +0530

    Time: 13 ms (84.21%), Space: 21.5 MB (98.72%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
@@ -12,15 +12,19 @@
 class Solution {
 public:
     bool isValidBST(TreeNode* root) {
+        int curr;
+        //inorder traversal mein bst is sorted so if we do a morris traversal we can get linear time
+        //constant space
         bool ans=true;
-        long long curr=LLONG_MIN;
+        bool init=false;
         while(root)
         {
             if(!root->left)
             {
-                if(root->val<=curr)ans=false;
+                if(root->val<=curr and init)ans=false;
                 curr=root->val;
-                root=root->right;
+                if(!init)init=true;
+                root=root->right;                
             }
             else
             {
@@ -29,19 +33,20 @@ public:
                 {
                     temp=temp->right;
                 }
-                if(!temp->right)
+                if(temp->right==root)
+                {
+                    temp->right=nullptr;
+                    if(root->val<=curr and init)ans = false;
+                    curr=root->val;
+                    if(!init)init=true;
+                    root=root->right;
+                }
+                else
                 {
                     temp->right=root;
                     root=root->left;
                 }
-                else
-                {
-                    temp->right=nullptr;
-                    if(root->val<=curr)ans=false;
-                    curr=root->val;
-                    root=root->right;
-                }
-            }
+            }            
         }
         return ans;
     }
