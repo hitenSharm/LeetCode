512f7dc730189fdea4c177aca62ab7795809804c
Hiten Sharma <56029311+hitenSharm@users.noreply.github.com>
2022-10-25 21:41:30 +0530

    Time: 87 ms (82.39%), Space: 48.2 MB (93.57%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,17 +1,11 @@
 class Solution {
 public:
-        
     bool canJump(vector<int>& nums) {
-        if(nums.size()<=1)return true;
-        //dp approach, at each point i can jump for the next jump[i] indexes
-        //i just need to know if i can reach end
-        //greedy approach is to see how max far i can jump when moving to 
-        //the max possible index = i+nums[i] where i is in prev range
-        int maxIndex=nums[0];
-        for(int i=1;i<=maxIndex;i++)
+        int maxiIndex=nums[0];
+        for(int i=0;i<=maxiIndex;i++)
         {
             if(i>=nums.size()-1)return true;
-            maxIndex=max(maxIndex,i+nums[i]);
+            maxiIndex=max(maxiIndex,nums[i]+i);
         }
         return false;
     }
