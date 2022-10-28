fa3c4a0d10a98ee62b3b0bd631dc1465e2868f5a
Hiten Sharma <56029311+hitenSharm@users.noreply.github.com>
2022-10-28 11:03:18 +0530

    Time: 132 ms (14.49%), Space: 30.8 MB (5.89%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        unordered_map<string,vector<string>>ump;
+        map<vector<int>,vector<string>>ump;
         for(auto i:strs)
         {
             vector<int>freq(26,0);
@@ -9,10 +9,7 @@ public:
             {
                 freq[j-'a']++;
             }
-            string temp="";
-            for(int j=0;j<26;j++)
-                temp+=to_string(freq[j])+',';            
-            ump[temp].push_back(i);
+            ump[freq].push_back(i);
         }
         vector<vector<string>>ans;
         for(auto it:ump)
